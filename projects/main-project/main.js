c568bbc254c14b1f312aefc426be684320d6b8ab
David Koen <51445963+DavidKoen@users.noreply.github.com>
2024-04-02 09:17:33 +0200

    cleaned main project

--- projects/main-project/main.js
+++ projects/main-project/main.js
@@ -107,23 +107,3 @@ document.querySelector("#push").addEventListener("click", () => {
         newTaskInput.value = "";
     }
 });
-
-
-
-function handleFormSubmit(event) {
-    event.preventDefault();
-
-    const data = new FormData(event.target);
-
-    const formJSON = Object.fromEntries(data.entries());
-
-    // for multi-selects, we need special handling
-    formJSON.snacks = data.getAll('snacks');
-
-    const results = document.querySelector('.results pre');
-    results.innerText = JSON.stringify(formJSON, null, 2);
-    console.log(JSON.stringify(formJSON));
-}
-
-const form = document.querySelector('.contact-form');
-form.addEventListener('submit', handleFormSubmit);
